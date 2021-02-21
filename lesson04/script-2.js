e336cde3efd53dbe91ede223340562c176c5e42e
deaddiana <deaddian@uw.edu>
2021-02-21 13:50:21 -0800

    added button to lesson 4

--- lesson04/script-2.js
+++ lesson04/script-2.js
@@ -1,8 +1,10 @@
 
 
-
+function getMultiplied() {
 
 let number1 = prompt('Enter a number: ');
 let number2 = prompt('Enter another number: ');
 
 alert('Hello, your numbers multiplied together is' + ' ' + number1 * number2);
+
+}
--- lesson04/script.js
+++ lesson04/script.js
@@ -1,5 +1,9 @@
 
+
+function getSquared() {
+
 let number = prompt('Enter a number: ');
 
+alert('Hello, your number squared is' + ' ' + number * number);
 
-alert('Hello, your squared is' + ' ' + number * number);
+}
